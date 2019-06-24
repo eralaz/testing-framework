986bf7b5b310f07476991ef7b01585adaab3806f
valera-I <lazarevuch@gmail.com>
2019-06-24 19:10:25 +0300

    add installation log

--- install/installer.sh
+++ install/installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e 'installing required services....'
+echo -e '\nShell test framework. Installing required services....\n'
 
 services2install=$(find -L "$(pwd)/services" -name "*.sh")
 
@@ -8,3 +8,5 @@ for service in $services2install
 do
     source service
 done
+
+echo -e "\nServices have been installed. Installation log can be found at /var/log/install.log\n"
--- install/template.sh
+++ install/template.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 service="service_name"
+echo -e "Installing $service ...\n"
 
 if [ $($service --version | grep "command not found") -gt 0 ]; then
     brew install $service
+    echo -e "$service has been installed\n"
+else
+    echo -e "$service is already installed!\n"
 fi
+
